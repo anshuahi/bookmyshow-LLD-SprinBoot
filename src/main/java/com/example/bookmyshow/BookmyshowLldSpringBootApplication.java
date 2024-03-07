442d9b5aa9d0f268d2ec1d70e6c057e622f974a5
Anshu Ahirwar <anshuahi@Anshus-MacBook-Air.local>
2024-03-08 02:31:02 +0530

    spring project initialise

---
+++ src/main/java/com/example/bookmyshow/BookmyshowLldSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowLldSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyshowLldSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmyshow/BookmyshowLldSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowLldSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
